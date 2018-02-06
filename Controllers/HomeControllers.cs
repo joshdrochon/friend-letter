6d58c235643a67b57893c18338ed923818239682
Josh Rochon <joshdrochon@gmail.com>
2018-02-06 13:44:30 -0800

    use controllers to generate dynamic html

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [Route("/form")]
+      public ActionResult Form()
+      {
+        return View();
+      }
+
+      [Route("/greeting_card")]
+      public ActionResult GreetingCard()
+      {
+          LetterVariable myLetterVariable = new LetterVariable();
+          myLetterVariable.SetRecipient(Request.Query["recipient"]);
+          myLetterVariable.SetSender(Request.Query["sender"]);
+          return View("Hello", myLetterVariable);
+      }
+
+
+      [Route("/")]
+      public ActionResult Hello()
+      {
+        LetterVariable myLetterVariable = new LetterVariable();
+        myLetterVariable.SetRecipient("Eric");
+        myLetterVariable.SetSender("Josh");
+        return View(myLetterVariable);
+      }
+
+      [Route("/letter")]
+      public ActionResult Letter()
+      {
+        return View();
+      }
+
+      [Route("/journal")]
+      public ActionResult Journal()
+      {
+        return View();
+      }
+
+      //[Produces("text/html")]
+      [Route("/goodbye")]
+      public string Goodbye()
+      {
+        return "Goodbye friend!";
+      }
+    }
+}
